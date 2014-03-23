1e9c3f235d78a740ca43b1c6c199e98ed0df5b06
Byron Heads <byron.heads@gmail.com>
2014-03-23 19:09:50 -0400

    Delete app.d
    Wont work with Dub and vibe.d

--- src/app.d
+++
@@ -1,8 +0,0 @@
-import temple.temple;
-
-version(unittest) {
-	pragma(msg, "Compiling templ with tests...");
-} else {
-	static assert(false, "Please build with -unittest when compiling directly");
-}
-void main() {}
